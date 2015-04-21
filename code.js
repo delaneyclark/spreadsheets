56fe26dba8bde92037e2f1dee99b63ebcca990cd
delaneyclark <delaneyclark6@gmail.com>
2015-04-20 20:56:23 -0400

    color boxes arranged

--- code.js
+++ code.js
@@ -17,105 +17,105 @@ $(document).ready(function(){
 				
 				  for (var i=0; i<data.length; i++) {
 				  	if (data[i].onascaleof0to9howcoldareyou == 0) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#44BBBB\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#44BBBB\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 1) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#53A6AC\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#53A6AC\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 2) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#62919D\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#62919D\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 3) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#717D8E\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#717D8E\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 4) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#80687F\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#80687F\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 5) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#90536F\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#90536F\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 6) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#9F3E60\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#9F3E60\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 7) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#AE2A51\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#AE2A51\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 8) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#BD1542\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#BD1542\"></th>" )
 						} else if (data[i].onascaleof0to9howcoldareyou == 9) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#CC0033\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#CC0033\"></th>" )
 						} else {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#CC0033\"></div>" )
+							$("tr.temp").append("<th id=\"temp\" style=\"background-color:#CC0033\"></th>" )
 						}
 					};
 
 					for (var i=0; i<data.length; i++) {
 				  	if (data[i].onascaleof0to9howgoodatsingingareyou == 0) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#FFFF33\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#FFFF33\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 1) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#E8E84A\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#E8E84A\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 2) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#D2D260\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#D2D260\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 3) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#BBBB77\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#BBBB77\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 4) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#A4A48E\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#A4A48E\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 5) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#8E8EA4\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#8E8EA4\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 6) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#7777BB\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#7777BB\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 7) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#6060D2\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#6060D2\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 8) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#4A4AE8\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#4A4AE8\"></th>" )
 						} else if (data[i].onascaleof0to9howgoodatsingingareyou == 9) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#3333FF\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#3333FF\"></th>" )
 						} else {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#3333FF\"></div>" )
+							$("tr.sing").append("<th id=\"sing\" style=\"background-color:#3333FF\"></th>" )
 						}
 					};
 
 					for (var i=0; i<data.length; i++) {
 				  	if (data[i].howareyou == 0) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#FFFFFF\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#FFFFFF\"></th>" )
 						} else if (data[i].howareyou == 1) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#E3E3E3\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#E3E3E3\"></th>" )
 						} else if (data[i].howareyou == 2) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#C6C6C6\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#C6C6C6\"></th>" )
 						} else if (data[i].howareyou == 3) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#AAAAAA\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#AAAAAA\"></th>" )
 						} else if (data[i].howareyou == 4) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#8E8E8E\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#8E8E8E\"></th>" )
 						} else if (data[i].howareyou == 5) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#717171\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#717171\"></th>" )
 						} else if (data[i].howareyou == 6) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#555555\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#555555\"></th>" )
 						} else if (data[i].howareyou == 7) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#393939\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#393939\"></th>" )
 						} else if (data[i].howareyou == 8) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#1C1C1C\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#1C1C1C\"></th>" )
 						} else if (data[i].howareyou == 9) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#000000\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#000000\"></th>" )
 						} else {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#000000\"></div>" )
+							$("tr.are").append("<th id=\"are\" style=\"background-color:#000000\"></th>" )
 						}
 					};
 
 					for (var i=0; i<data.length; i++) {
 				  	if (data[i].whereareyou == 0) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#7382F4\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#7382F4\"></th>" )
 						} else if (data[i].whereareyou == 1) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#6675E2\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#6675E2\"></th>" )
 						} else if (data[i].whereareyou == 2) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#5967D0\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#5967D0\"></th>" )
 						} else if (data[i].whereareyou == 3) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#4D5ABF\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#4D5ABF\"></th>" )
 						} else if (data[i].whereareyou == 4) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#404DAD\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#404DAD\"></th>" )
 						} else if (data[i].whereareyou == 5) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#333F9B\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#333F9B\"></th>" )
 						} else if (data[i].whereareyou == 6) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#263289\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#263289\"></th>" )
 						} else if (data[i].whereareyou == 7) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#1A2578\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#1A2578\"></th>" )
 						} else if (data[i].whereareyou == 8) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#0D1766\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#0D1766\"></th>" )
 						} else if (data[i].whereareyou == 9) {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#000A54\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#000A54\"></th>" )
 						} else {
-							$("table.temp").append("<div id=\"temp\" style=\"background-color:#000A54\"></div>" )
+							$("tr.where").append("<th id=\"where\" style=\"background-color:#000A54\"></th>" )
 						}
 					};
 				  };
